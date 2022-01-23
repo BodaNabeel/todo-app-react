8313d6526ddacdbe1df3dd18336bdc9230c84a13
BodaNabeel <needtoexplore01@gmail.com>
2022-01-23 20:53:33 +0530

    added delete functionality

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
+import { MdDelete } from "react-icons/md";
 
 function TodoList({ todos, setTodos }) {
+  const deleteTask = (id) => {
+    const updatedTodos = todos.filter((item) => {
+      return id !== item.id;
+    });
+
+    // console.log(updatedTodos)
+    setTodos(updatedTodos);
+  };
   const changeTaskState = (id) => {
-    const updatedTodos = todos.map((item, index) => {
-      console.log(item);
+    const updatedTodos = todos.map((item) => {
       if (id === item.id) {
         return { ...item, isCompleted: !item.isCompleted };
       }
@@ -28,6 +36,7 @@ function TodoList({ todos, setTodos }) {
               type="checkbox"
               onClick={(event) => changeTaskState(item.id)}
             />
+            <MdDelete key={item.id} onClick={(event) => deleteTask(item.id)} />
           </li>
         );
       })}
