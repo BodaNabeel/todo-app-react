5010817bf18287bd0ad4db5babad106e625ff767
BodaNabeel <needtoexplore01@gmail.com>
2022-01-23 22:49:06 +0530

    code shifted to todolistitem file

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,44 +1,11 @@
 import React, { useState } from "react";
-import { MdDelete } from "react-icons/md";
+import TodoListItem from "./TodoListItem";
 
 function TodoList({ todos, setTodos }) {
-  const deleteTask = (id) => {
-    const updatedTodos = todos.filter((item) => {
-      return id !== item.id;
-    });
-
-    // console.log(updatedTodos)
-    setTodos(updatedTodos);
-  };
-  const changeTaskState = (id) => {
-    const updatedTodos = todos.map((item) => {
-      if (id === item.id) {
-        return { ...item, isCompleted: !item.isCompleted };
-      }
-      return item;
-    });
-    setTodos(updatedTodos);
-  };
   return (
     <ul>
-      {todos.map((item) => {
-        return (
-          <li
-            key={item.id}
-            style={
-              item.isCompleted
-                ? { textDecoration: "line-through" }
-                : { textDecoration: "none" }
-            }
-          >
-            {item.task}
-            <input
-              type="checkbox"
-              onClick={(event) => changeTaskState(item.id)}
-            />
-            <MdDelete key={item.id} onClick={(event) => deleteTask(item.id)} />
-          </li>
-        );
+      {todos.map((todo) => {
+        return <TodoListItem key={todo.id} todos={todos} todo={todo} setTodos={setTodos}/>;
       })}
     </ul>
   );
