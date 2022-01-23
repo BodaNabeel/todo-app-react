7d26a59aab0997d1d6c7e50a8dcf1092c50d7ad9
BodaNabeel <needtoexplore01@gmail.com>
2022-01-23 22:48:08 +0530

    TodoListItems separately added

---
+++ src/components/TodoListItem.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useRef } from "react";
+import { MdDelete } from "react-icons/md";
+import { BiEditAlt } from "react-icons/bi";
+
+function TodoListItem({ todos, setTodos, todo }) {
+  const taskInput = useRef();
+  const [taskName, settaskName] = useState(todo.task);
+
+  const editTaskName = (id, value) => {
+    settaskName(value);
+    const updatedTodos = todos.map((item) => {
+      if (id === item.id) {
+        return { ...item, task: value };
+      }
+      return item;
+    });
+    console.log(updatedTodos);
+  };
+  const enableEdit = () => {
+    taskInput.current.disabled = false;
+
+    taskInput.current.focus();
+  };
+  const deleteTask = (id) => {
+    const updatedTodos = todos.filter((item) => {
+      return id !== item.id;
+    });
+    setTodos(updatedTodos);
+  };
+  const changeTaskState = (id) => {
+    const updatedTodos = todos.map((item) => {
+      if (id === item.id) {
+        return { ...item, isCompleted: !item.isCompleted };
+      }
+      return item;
+    });
+    setTodos(updatedTodos);
+  };
+  return (
+    <li
+      key={todo.id}
+      style={
+        todo.isCompleted
+          ? { textDecoration: "line-through" }
+          : { textDecoration: "none" }
+      }
+    >
+      <input
+        ref={taskInput}
+        className="task-input"
+        disabled
+        onChange={(event) => {
+          editTaskName(todo.id, event.target.value);
+        }}
+        value={taskName}
+      />
+      <div className="buttons">
+        <input type="checkbox" onClick={(event) => changeTaskState(todo.id)} />
+        <MdDelete key={todo.id} onClick={(event) => deleteTask(todo.id)} />
+        <BiEditAlt onClick={enableEdit} />
+      </div>
+    </li>
+  );
+}
+
+export default TodoListItem;
